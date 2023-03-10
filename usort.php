b857dc3bdf3c8e5285bb70af82383069255a054d
Marco Antônio <markomasf@gmail.com>
2023-03-10 14:01:38 -0300

    usort

---
+++ usort.php
@@ -0,0 +1,21 @@
+<?php
+
+$notas = [
+    [
+        'aluno' => "Marco",
+        'nota' => 9,
+    ],
+    [
+        'aluno' => 'Lorene',
+        'nota' => 10,
+    ],
+    [
+        'aluno' => 'Larissa',
+        'nota' => 8,
+    ],
+    [
+        'aluno' => 'Neia',
+        'nota' => 7,
+    ],
+
+];
