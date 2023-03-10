6c1b5dbfade0a034e2ff4b3ae4eceff74ba48b5d
Marco Antônio <markomasf@gmail.com>
2023-03-10 14:47:29 -0300

    ksort

---
+++ notas.php
@@ -0,0 +1,19 @@
+<?php
+
+$notas = [
+    'Marco' => 10,
+    'Lorene' => 8,
+    'João' => 9,
+    'Pedro' => 6,
+    'Carlos' => 7,
+];
+
+ksort($notas);
+var_dump($notas);
+
+/*
+asorte (ordena sem mudar a chave)
+
+ksorte (ordena pela chave)
+
+*/
