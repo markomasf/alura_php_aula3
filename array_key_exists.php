9dca4ed904a6fe4a169dbe7d678d615e94c102e7
Marco Antônio <markomasf@gmail.com>
2023-03-13 11:20:08 -0300

    isset e arryakeyexists

--- array_key_exists.php
+++ array_key_exists.php
@@ -1,37 +1,20 @@
 <?php
 
 $notas = [
-    [
-        'aluno' => 'Marco',
-        'nota' => 9,
-    ],
-    [
-        'aluno' => 'Lorene',
-        'nota' => 10,
-    ],
-    [
-        'aluno' => 'Larissa',
-        'nota' => 7,
-    ],
-    [
-        'aluno' => 'Neia',
-        'nota' => 8,
-    ],
-
+    'Marco' => null,
+    'Lorene' => 10,
+    'Layane' => 5,
+    'Larissa' => 9,
+    'Lucineia' => 8,
+    'Marcos' => 7,
+    'Matheus' => 6,
+    'Bruno' => 4
 ];
-
-usort($notas, 'OrdenarDecrescente');
-
-function OrdenarDecrescente($nota1, $nota2){
-    if($nota1['nota'] > $nota2['nota']){
-        return -1;
-    }
-    if($nota2['nota'] > $nota1['nota']){
-        return 1;
-    }
-    return 0;
-}
 
 //var_dump($notas);
 
-var_dump(array_key_exists('4', $notas));
+echo "O aluno fez a prova: ";
+var_dump(array_key_exists('Marco', $notas));
+
+// isset (Valida se o campo está com NULL)
+var_dump(isset($notas['Lorene']));
--- usort.php
+++ usort.php
@@ -23,13 +23,15 @@ $notas = [
 usort($notas, 'OrdenarDecrescente');
 
 function OrdenarDecrescente($nota1, $nota2){
-    if($nota1['nota'] > $nota2['nota']){
+    /* if($nota1['nota'] > $nota2['nota']){
         return -1;
     }
     if($nota2['nota'] > $nota1['nota']){
         return 1;
     }
-    return 0;
+    return 0; */ //substituido pelo comando abaixo:
+    return $nota1['nota'] <=> $nota2['nota'];
+
 }
 
 var_dump($notas);
