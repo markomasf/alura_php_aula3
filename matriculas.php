2645e1f33ba49fada8cbe2be39e71cd94b57cde2
Marco Antonio <markomasf@gmail.com>
2023-03-14 11:15:17 -0300

    array_push e array_unshift

--- matriculas.php
+++ matriculas.php
@@ -14,8 +14,19 @@ $novosAlunos = [
     'Bruno'
 ];
 
+// array_merge faz as junção entre os array
 $alunos2022 = array_merge($alunos2021, $novosAlunos);
 
+//nessa sintaxe "..." é possível colocar informaçções entre os valores.
 $alunos2023 = [...$alunos2021, 'Marco Fonseca', ...$novosAlunos];
+
+//para adicionar novos elementos ao array
+array_push($alunos2023, 'Joao', 'José', 'Maria');
+
+//outra forma de adicionar elementos
+$alunos2023[] = 'Carlos';
+
+//para adicionar os elementos no começo da array
+array_unshift($alunos2023, 'Mario', 'Rafael');
 
 var_dump($alunos2023);
